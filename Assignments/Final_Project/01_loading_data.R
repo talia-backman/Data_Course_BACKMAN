8fc3c03278af7248ae31c77c7b1b8c9568174734
talia-backman <talia.backman@gmail.com>
2020-04-26 17:21:30 -0600

    final project

--- Assignments/Final_Project/01_loading_data.R
+++ Assignments/Final_Project/01_loading_data.R
@@ -1,41 +1,59 @@
 library(dada2)
+library(purrr)
+library(tidyr)
+library(ggplot2)
+library(readxl)
 
 path <- "./PlateData" 
 list.files(path)
-
-fnFs <- sort(list.files(path, pattern="R1_001.fastq", full.names = TRUE))
+                
+fnFs <- sort(list.files(path, pattern="R1_001.fastq", full.names = TRUE, recursive = TRUE))
 fnFs
-fnRs <- sort(list.files(path, pattern="R2_001.fastq", full.names = TRUE))
+fnRs <- sort(list.files(path, pattern="R2_001.fastq", full.names = TRUE, recursive = TRUE))
 # Extract sample names
 sample.names <- sapply(strsplit(basename(fnFs), "_"), `[`, 1)
-
-plotQualityProfile(fnFs[1:9])
-plotQualityProfile(fnRs[1:9])
-
-
+sample.names
+                
+plotQualityProfile(fnFs[1:2])
+plotQualityProfile(fnRs[1:2])
+                
+                
 # Place filtered files in filtered/ subdirectory
-filtFs <- file.path(path, "filtered", paste0(sample.names, "_F_filt.fastq.gz"))
-filtRs <- file.path(path, "filtered", paste0(sample.names, "_R_filt.fastq.gz"))
+filtFs <- file.path(path, "filtered", paste0(sample.names, "_F_filt.fastq"))
+filtRs <- file.path(path, "filtered", paste0(sample.names, "_R_filt.fastq"))
 names(filtFs) <- sample.names
 names(filtRs) <- sample.names
-
-out <- filterAndTrim(fnFs, filtFs, fnRs, filtRs, truncLen=c(240,160),
-                     maxN=0, maxEE=c(2,2), truncQ=2, rm.phix=TRUE,
-                     compress=TRUE, multithread=TRUE) 
+                
+out <- filterAndTrim(fnFs, filtFs, fnRs, filtRs,
+                                     maxN=0, maxEE=c(2,2), truncQ=2, rm.phix=TRUE,
+                                     compress=TRUE, multithread=TRUE) 
 head(out)
-
+                
 errF <- learnErrors(filtFs, multithread=TRUE)
 errR <- learnErrors(filtRs, multithread=TRUE)
-
+                
 plotErrors(errF, nominalQ=TRUE)
 plotErrors(errR, nominalQ=TRUE)
-
-dadaFs <- dada(filtFs, err=errF, multithread=TRUE)
-dadaRs <- dada(filtRs, err=errR, multithread=TRUE)
-
+                
+derepFs <- derepFastq(filtFs, verbose=TRUE)
+derepRs <- derepFastq(filtRs, verbose=TRUE)
+# Name the derep-class objects by the sample names
+names(derepFs) <- sapply(strsplit(basename(filtFs), "_"), `[`, 1)
+names(derepRs) <- sapply(strsplit(basename(filtRs), "_"), `[`, 1)
+                
+dadaFs <- dada(derepFs, err=errF, multithread=TRUE)
+dadaRs <- dada(derepRs, err=errR, multithread=TRUE)
+                
 dadaFs[[1]]
+                
+seqtab <- makeSequenceTable(dadaFs)
+dim(seqtab)
 
-mergers <- mergePairs(dadaFs, filtFs, dadaRs, filtRs, verbose=TRUE)
-
-# Inspect the merger data.frame from the first sample
-head(mergers[[1]])
+#save seqtab and dada as RDS
+saveRDS(dadaFs, "./filtered/dadaFs.RDS")
+saveRDS(dadaRs, "./filtered/dadaRs.RDS")
+saveRDS(derepFs, "./filtered/derepFs.RDS")
+saveRDS(derepRs, "./filtered/derepRs.RDS")
+saveRDS(errF, "./filtered/errF.RDS")
+saveRDS(errR, "./filtered/errR.RDS")
+saveRDS(seqtab, "./filtered/seqtab.RDS")
--- Assignments/Final_Project/02_seq_align.R
+++ Assignments/Final_Project/02_seq_align.R
@@ -1,7 +1,18 @@
 library(dada2)
 library(msa)
 
-mergers <- readRDS("./filtered/mergers.RDS")
+seqtab <- readRDS("./filtered/seqtab.RDS")
+
+uniq_seqtab<- getUniques(seqtab, collapse = TRUE, silence = FALSE)
+uniquesToFasta(uniq_seqtab, "./filtered/uniq_seqtab.fas")
+head(uniq_seqtab)
+
+seqs <- readDNAStringSet("./filtered/uniq_seqtab.fas", format="fasta")
+seqs
+
+align <- msa(seqs, method = "Muscle")
+
+
 getSequences()
 ShortRead::writeFasta(fqs,"./test.fasta")
 
